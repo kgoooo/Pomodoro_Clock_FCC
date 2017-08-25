b28b913bccbf5b133281a5f67f13c7d8b08d7c00
Kristen Goehringer <kgoehringer@gmail.com>
2017-08-25 20:13:42 +0200

    starting to build, no functionality, minor styling.

---
+++ script.js
@@ -0,0 +1,27 @@
+// FlipClock.SessionClock = FlipClock.Face.extend({
+	
+// 	build: function() {
+// 		this.log('This method is used to build your clock face.');
+// 	},
+	
+// 	reset: function() {
+// 		this.log('This method is triggered when the clock is reset');
+// 	},
+	
+// 	start: function() {
+// 		this.log('This method is triggered when the clock is started');
+// 	},
+	
+// 	stop: function() {
+// 		this.log('This method is triggered when the clock is stopped');
+// 	},
+	
+// 	increment: function() {
+// 		this.log('This method is used to increment your clock face.');
+// 	},
+
+// 	flip: function() {
+// 		this.log('This method is triggered when the clock is flipped');
+// 	}
+	
+// });
